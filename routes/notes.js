9ed48f673bc4af629cc07a5395d48b870be06812
sonjdaniel <sonjdaniel@gmail.com>
2023-05-21 17:26:45 -0500

    testing out nodeman and note route

--- routes/notes.js
+++ routes/notes.js
@@ -1,7 +1,26 @@
 const router = require("express").Router();
 const path = require("path");
+// const path = require('path');
 const notes = require("../db/db.json");
+const { v4: uuidv4 } = require("uuid");
+const fs = require("fs");
 
 router.get("/api/notes", (req, res) => res.json(notes));
+
+router.post("/api/notes", (req, res) => {
+  const { title, text } = req.body;
+
+  const newNote = {
+    title,
+    text,
+    // id: uuidv4(),
+  };
+
+  const noteString = JSON.stringify(newNote);
+
+  fs.appendFile(notes, noteString, (err) =>
+    err ? console.error(err) : console.log("Done")
+  );
+});
 
 module.exports = router;
