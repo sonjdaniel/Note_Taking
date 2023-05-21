c7a34cf6ac588e02fafc4b6e63293b341f71bc73
sonjdaniel <sonjdaniel@gmail.com>
2023-05-21 17:38:44 -0500

    add the router for note

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,9 @@
 const express = require("express");
 // Import modular router for /notes
 const notesRouter = require("./notes");
+
+const app = express();
+// Using notes router with /api/notes endpoint added
+app.use("/api/notes", notesRouter);
+
+module.exports = app;
