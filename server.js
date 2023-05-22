cca6d321a632926637358246cefd37b4976f4f53
sonjdaniel <sonjdaniel@gmail.com>
2023-05-21 20:18:23 -0500

    change the server .js

--- server.js
+++ server.js
@@ -1,28 +1,25 @@
-// Importing Express.js
 const express = require("express");
-// Importing Node.js path module
 const path = require("path");
-const app = express();
-// Importing index.js
-const api = require("./routes/index");
-const PORT = process.env.PORT || 3000;
+const api = require("./routes/index.js");
 
-// Middleware for parsing JSON and urlencoded form data
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/api", api);
 
 app.use(express.static("public"));
-app.use(api);
 
-// GET Route for notes page
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/index.html"))
+);
+
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
-// GET Route for main page (using wildcard)
-app.get("/*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/index.html"))
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} `)
 );
-
-// Server listening on port 3000
-app.listen(PORT);
